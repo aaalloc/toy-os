a229caa64fa153c3654b080fb4ce44dd8d1b7937
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2024-07-20 23:42:36 +0200

    trying things for floating register

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -1,6 +1,13 @@
+# .section .data
+# float_val: .float 23.1    
+    .attribute arch, "rv64gc"
     .section .text.entry
     .globl _start
 _start:
+    # do little arithmetic with floating point numbers
+    # la a0, float_val  # Load address of float_val into a0
+    # flw ft0, 0(a0)    # Load the floating-point value into ft0
+
     la sp, boot_stack_top
     call kmain
 
