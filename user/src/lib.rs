379edc7c6f8d286a45a511fe95ab71021d67b03f
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2024-07-28 10:55:52 +0200

    cooking for cat

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -11,6 +11,9 @@ mod syscall;
 use bitflags::bitflags;
 use buddy_system_allocator::LockedHeap;
 
+extern crate alloc;
+use alloc::vec::Vec;
+
 const USER_HEAP_SIZE: usize = 16384;
 
 static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
@@ -25,12 +28,26 @@ pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
 
 #[no_mangle]
 #[link_section = ".text.entry"]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(argc: usize, argv: usize) -> ! {
     unsafe {
         HEAP.lock()
             .init(HEAP_SPACE.as_ptr() as usize, USER_HEAP_SIZE);
     }
-    exit(main());
+    let mut v: Vec<&'static str> = Vec::new();
+    for i in 0..argc {
+        let str_start =
+            unsafe { ((argv + i * core::mem::size_of::<usize>()) as *const usize).read_volatile() };
+        let len = (0usize..)
+            .find(|i| unsafe { ((str_start + *i) as *const u8).read_volatile() == 0 })
+            .unwrap();
+        v.push(
+            core::str::from_utf8(unsafe {
+                core::slice::from_raw_parts(str_start as *const u8, len)
+            })
+            .unwrap(),
+        );
+    }
+    exit(main(argc, v.as_slice()));
     panic!("unreachable after sys_exit!");
 }
 
@@ -42,7 +59,7 @@ pub struct TimeVal {
 
 #[linkage = "weak"]
 #[no_mangle]
-fn main() -> i32 {
+fn main(_argc: usize, _argv: &[&str]) -> i32 {
     panic!("Cannot find main!");
 }
 bitflags! {
